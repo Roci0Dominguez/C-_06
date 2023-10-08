b4dc7f70140d4c70d933b59207ca196c281ade29
Jorge <Jorge@DESKTOP-7L78HFA>
2023-10-07 23:46:58 -0600

    Agregar archivos de proyecto.

---
+++ vector_areglo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Declara una lista de cadenas
+        string[] aves = { "loro gris", "paloma de diamante", "coctel" };
+
+        Console.WriteLine("Los valores de la lista antes de insertar:");
+        foreach (string ave in aves)
+        {
+            Console.Write(ave + " ");
+        }
+
+        Console.WriteLine("\n");
+
+        // Agrega tres valores al final de la lista utilizando el método Array.Resize()
+        Array.Resize(ref aves, aves.Length + 3);
+        aves[aves.Length - 3] = "mayna";
+        aves[aves.Length - 2] = "periquitos";
+        aves[aves.Length - 1] = "cacatua";
+
+        Console.WriteLine("Los valores de la lista después de insertar:");
+        foreach (string ave in aves)
+        {
+            Console.Write(ave + " ");
+        }
+
+        Console.WriteLine("\n");
+    }
+}
