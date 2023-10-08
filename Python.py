860d44f31e10005b09624441ca0f4597852478b3
Roci0Dominguez <144483622+Roci0Dominguez@users.noreply.github.com>
2023-10-07 23:50:33 -0600

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,20 @@
+#declara una lista de cadenas
+aves=["loro gris","paloma de diamante","coctel"]
+print("los valores de la lista antes de insertar:")
+for ave in aves:
+       print(ave, end=" ")
+
+print("\n")
+
+      #agrega tres valores al final de la lista utilizando el metodo append()
+aves.append("mayna")
+aves.append("periquitos")
+aves.append("cacatua")
+print("los valores de la lista despues de insertar:")
+      # itera sobre la lista para imprimir los valores
+for ave in aves:
+ print(ave, end=" ")
+
+print("\n")
+
+#Yareli del Rocio Perez Dominguez
